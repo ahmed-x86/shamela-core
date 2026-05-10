6ce7169d82e598ddf13c15cea55813f591ec54d4
ahmed-x86 <ahmdansmr7@gmail.com>
2026-05-10 22:18:29 +0300

    .

--- launch.sh
+++ launch.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-
+# Define paths
 BASE_DIR="/opt/shamela"
 APP_BIN_DIR="$BASE_DIR/app/linux/64/bin"
-
+# Change to base directory
 cd "$BASE_DIR" || exit
-
+# Execute the application with required environment variables
 exec env LD_LIBRARY_PATH="$APP_BIN_DIR:$LD_LIBRARY_PATH" \
          QT_API=pyside2 \
          QT_QPA_PLATFORM=xcb \
